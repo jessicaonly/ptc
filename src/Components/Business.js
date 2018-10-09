8e99daf3e31e23c6d1e9138f0259992fb12a32bb
Jessica Skok <jessica.skok@gmail.com>
2018-10-09 01:44:38 -0700

    working on photo stuff

--- src/Components/Business.js
+++ src/Components/Business.js
@@ -11,25 +11,38 @@ class Business extends React.Component {
   }
 
   componentDidMount = async () => {
-    const indiividualName = this.props.location.state.business;
+    const individualName = this.props.location.state.business;
     const req = 
-    await fetch(`http://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?placeid=${indiividualName}&key=${API_KEY}`);
+    await fetch(`http://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?placeid=${individualName}&key=${API_KEY}`);
     const res = await req.json();
     this.setState({activeBusiness: res.result})
     console.log(this.state.activeBusiness);
 
     const tempPhoto = 'https://www.jainsusa.com/images/store/landscape/not-available.jpg';
+    const photo = '';
     if (this.state.activeBusiness.photos.length > 0) {
       const actualPhoto = await fetch(`http://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${this.state.activeBusiness.photos[0].photo_reference}&key=${API_KEY}`);
-      const photo = await actualPhoto.getUrl();
-      this.setState({picture: photo});
+      photo = await actualPhoto;
+      let picture = Object.assign({}, this.state.picture);
+      picture = photo;
+      return this.setState({picture});
     }
+  
     else {
-    this.setState({picture: tempPhoto});
+      photo = tempPhoto;
     }
     console.log(this.state.picture)
   
-}
+
+
+    // this.state.businesses.map = async (business) => {
+    // const photo_call = await fetch(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${business.photos[0].photo_reference}&key=${API_KEY}`);
+    // photos = await photo_call;
+    //   return photos;
+    // }
+    // console.log(photos);
+    // console.log(this.state.businesses);
+  }
   
     
 
@@ -39,7 +52,7 @@ class Business extends React.Component {
    return (
      <div className="container">
       <div className="active-business">
-      <div className="active-business__img" src={this.state.picture} alt={business.place_id}/>
+      <img className="active-business__img" src = {this.state.picture} alt={business.place_id}/>
       </div>
      </div>
    );
